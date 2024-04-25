878da113ee788615233289135f8975849021e1fa
brenocrepaldi <brenogaia2004@gmail.com>
2024-04-25 09:15:26 -0300

    arquivos deletados

--- backend/services/helpers.py
+++
@@ -1,9 +0,0 @@
-# helpers.py
-def normalize_ratings(ratings):
-    # Função para normalizar as classificações
-    pass
-
-
-def filter_products_by_criteria(df_info, criteria):
-    # Função para filtrar produtos com base em critérios
-    pass
