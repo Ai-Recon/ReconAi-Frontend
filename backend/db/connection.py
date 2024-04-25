fe333af647dc400a1f6137e1fca70904c468f3c1
brenocrepaldi <brenogaia2004@gmail.com>
2024-04-25 09:33:18 -0300

    transformação da consulta no banco para dataframe

--- backend/db/connection.py
+++ backend/db/connection.py
@@ -1,47 +1,72 @@
 import os
 import time
 
+import pandas as pd
 import psycopg2
 from dotenv import load_dotenv
 
-# Carregando as variáveis de ambiente
-load_dotenv()
 
-start_time = time.time()
+# Conexão com o Postgres
+def connect_to_database():
+    try:
+        conn = psycopg2.connect(
+            host=os.getenv("HOST"),
+            database=os.getenv("DATABASE"),
+            user=os.getenv("USER"),
+            password=os.getenv("PASSWORD"),
+        )
+        return conn
 
-# Conectando ao banco de dados
-try:
-    conn = psycopg2.connect(
-        host=os.getenv("HOST"),
-        database=os.getenv("DATABASE"),
-        user=os.getenv("USER"),
-        password=os.getenv("PASSWORD"),
-    )
-    # Criando um cursor para executar consultas SQL
-    cursor = conn.cursor()
+    except psycopg2.Error as e:
+        print("Erro ao conectar ao PostgreSQL:", e)
+        return None
 
-    # Consulta SQL para selecionar todos os dados da tabela "produtos"
-    query = "SELECT * FROM produtos;"
 
-    # Executando a consulta SQL
-    cursor.execute(query)
+def fetch_data(conn):
+    if conn:
+        try:
+            cursor = conn.cursor()
+            query = "SELECT * FROM produtos;"
+            cursor.execute(query)
+            produtos = cursor.fetchall()
+            cursor.close()
+            return produtos
+
+        except psycopg2.Error as e:
+            print("Erro ao executar a consulta SQL:", e)
+            return None
+    else:
+        print("Erro com a conexão", conn)
+        return None
+
+
+def main():
+    # Carregando as variáveis de ambiente
+    load_dotenv()
+
+    start_time = time.time()
+
+    # Conectando ao banco de dados
+    conn = connect_to_database()
 
     # Recuperando os resultados da consulta
-    produtos = cursor.fetchall()
+    produtos = fetch_data(conn)
 
-    # Exibindo os resultados
-    for produto in produtos:
-        print(produto)
+    if produtos:
+        # Convertendo para DataFrame
+        df = pd.DataFrame(
+            produtos, columns=["ID", "Titulo", "Preco", "Classificacao", "Cor"]
+        )
+        print(df)
 
-    # Fechando o cursor e a conexão
-    cursor.close()
-    conn.close()
+    # Fechando a conexão
+    if conn:
+        conn.close()
 
-    # Fim da contagem de tempo
     end_time = time.time()
-    # Calculando o tempo total de execução
     execution_time = end_time - start_time
     print(f"Tempo de execução: {execution_time:.4f} segundos")
 
-except psycopg2.Error as e:
-    print("Erro ao conectar ao PostgreSQL:", e)
+
+if __name__ == "__main__":
+    main()
