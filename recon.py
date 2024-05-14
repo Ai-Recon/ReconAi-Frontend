269cf786044fc6b6e6d1ecce01a8db8d2788f7b5
JoaoCustodio66 <joaocustodiokintal@gmail.com>
2024-05-14 17:30:26 -0300

    tela inicio

--- recon.py
+++ recon.py
@@ -7,7 +7,6 @@ from backend.src.main_csv import get_recommendations
 # carrega com os dados do banco
 # from backend.src.main_db import get_recommendations
 
-
 app = Flask(__name__, static_folder="static")
 
 app.secret_key = "PUC"
@@ -15,14 +14,12 @@ app.secret_key = "PUC"
 # Carregar os dados pré-processados e calculados de main.py
 # df_recomendacoes = pd.read_csv("backend\\src\\db\\database\\produtos.csv")
 
-
 # Defina uma rota para processar a solicitação dos usuários e exibir as recomendações
 @app.route("/")
 def homepage():
     min_price = 0
     max_price = 1000
     return render_template("home.html", min_price=min_price, max_price=max_price)
-
 
 # Defina uma rota para processar a solicitação dos usuários e exibir as recomendações
 @app.route("/usuarios", methods=["GET", "POST"])
@@ -51,6 +48,9 @@ def usuarios():
         # Se for uma solicitação GET, retorne a página HTML
         return render_template("usuarios.html")
 
+@app.route("/inicio")
+def inicio():
+    return render_template("inicio.html")
 
 # @app.route(
 #     "/autenticar",
